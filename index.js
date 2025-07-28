f903d461dce20e4dfe9134b0bd9e4e20ad5fb691
alisonkwork <156113052+alisonkwork@users.noreply.github.com>
2025-07-28 20:01:39 +0000

    Beginning of MBN Case Study

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-ScrollReveal().reveal('.portfolioMBN', {delay: 100, origin: 'bottom', distance:'50px', duration: 1500});
-ScrollReveal().reveal('.portfolioTHERAPY', {delay: 100, origin: 'bottom', distance:'50px', duration: 1500});
-ScrollReveal().reveal('.portfolioHARVEST', {delay: 100, origin: 'bottom', distance:'50px', duration: 1500});
-ScrollReveal().reveal('.portfolioSEE', {delay: 100, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.portfolioMBN', {delay: 200, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.portfolioTHERAPY', {delay: 250, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.portfolioHARVEST', {delay: 300, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.portfolioSEE', {delay: 350, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.hero', {delay: 100, duration: 2000});
+ScrollReveal().reveal('.hero h2', {delay: 100, origin: 'bottom', distance:'10px', duration: 2000});
