b61f6d7f62b6ce0aa860bf2fbd810022e1b96fd9
alisonkwork <156113052+alisonkwork@users.noreply.github.com>
2025-07-28 16:49:41 +0000

    All portfolio cards

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Add JavaScript below
+ScrollReveal().reveal('.portfolioMBN', {delay: 100, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.portfolioTHERAPY', {delay: 100, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.portfolioHARVEST', {delay: 100, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.portfolioSEE', {delay: 100, origin: 'bottom', distance:'50px', duration: 1500});
