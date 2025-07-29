9284e6bc76385e4105a4fb1d515f3a40e0b51f76
alisonkwork <156113052+alisonkwork@users.noreply.github.com>
2025-07-29 01:39:02 +0000

    Made homepage responsive

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-ScrollReveal().reveal('.portfolioMBN', {delay: 200, origin: 'bottom', distance:'50px', duration: 1500});
-ScrollReveal().reveal('.portfolioTHERAPY', {delay: 250, origin: 'bottom', distance:'50px', duration: 1500});
-ScrollReveal().reveal('.portfolioHARVEST', {delay: 300, origin: 'bottom', distance:'50px', duration: 1500});
-ScrollReveal().reveal('.portfolioSEE', {delay: 350, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.portfolioCard', {delay: 200, origin: 'bottom', distance:'50px', duration: 1500});
 ScrollReveal().reveal('.hero', {delay: 100, duration: 2000});
 ScrollReveal().reveal('.hero h2', {delay: 100, origin: 'bottom', distance:'10px', duration: 2000});
