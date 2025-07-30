5f3151c62594f6dd554525a11af7ec56f8944b90
alisonkwork <156113052+alisonkwork@users.noreply.github.com>
2025-07-30 20:01:01 +0000

    Finished MBN Page

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 ScrollReveal().reveal('.portfolioCard', {delay: 200, origin: 'bottom', distance:'50px', duration: 1500});
 ScrollReveal().reveal('.hero', {delay: 100, duration: 2000});
 ScrollReveal().reveal('.hero h2', {delay: 100, origin: 'bottom', distance:'10px', duration: 2000});
+ScrollReveal().reveal('.previousNext', {delay: 100, duration: 2000});
+ScrollReveal().reveal('.portfolioHeader', {delay: 100, duration: 2000});
+ScrollReveal().reveal('.subNav', {delay: 0, duration: 2000});
+ScrollReveal().reveal('.portfolioSection', {delay: 100, duration: 2000});
+ScrollReveal().reveal('.cropped-image', {delay: 200, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.cropped-image-long', {delay: 200, origin: 'bottom', distance:'50px', duration: 1500});
+ScrollReveal().reveal('.cropped-image-large', {delay: 200, origin: 'bottom', distance:'50px', duration: 1500});
+
+
+let mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
